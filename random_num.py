02fd3bab3a68671db3264eb629484f0a9e738a21
lliu-regulen <lliu@regulen.com>
2024-11-25 16:17:48 -0500

    random_num

---
+++ random_num.py
@@ -0,0 +1,10 @@
+import random
+
+# Randomly select a section from 1 to 6
+section = random.randint(1, 6)
+
+# Randomly select a number from 1 to 120
+number = random.randint(1, 150)
+
+print(f"Randomly selected section: {section}")
+print(f"Randomly selected number: {number}")
